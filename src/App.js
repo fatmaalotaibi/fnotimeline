d5009f7f1ac02e9a02f2c5fb2cc81dded1671b70
FN Otaibi <nsayemlil@hotmail.com>
2020-07-01 16:12:21 +0300

    fatma timeline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+//Data
+import lifeTimeLine from "./data";
 
 function App() {
+  console.log(lifeTimeLine[0].name);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1> Fatma's TimeLine</h1>
+        <h4>A Journey Of Thousand Miles Begins With A Single Step</h4>
+        <img
+          alt="My way"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdX9QB02idCl-t9udR-UiciHo-g1VdUI1ItQ&usqp=CAU"
+        />
+      </div>
+      <div>
+        <div>
+          <p>I was born in 1985</p>
+          <p>My first year in school was in 1991 </p>
+          <p>I graduated from High School in 2003</p>
+          <p> I got married in 2004</p>
+          <p>In 2004 I started learning IT in Shall Institute</p>
+          <p>
+            In 2005 I gave birth to the most handsome boy in the whole world,
+            Mohammed
+          </p>
+          <p>In 2006 I gave birth to my little princes Rayan</p>
+          <p>I graduated in 2007 with a diploma in Computer Skills</p>
+          <p>
+            In 2008 I started working in the Ministry of Social Affairs and
+            Labor
+          </p>
+          <p>In 2013 I joined Arab Open University to study business</p>
+          <p>
+            I changed my work place and I joined the Public Authority of Sports
+          </p>
+          <p>
+            I graduated in 2017 with a Bachelor’s in Business Administration{" "}
+          </p>
+          <p>In 2019 I decided to learn coding and I started with Swift</p>
+          <p>In 2020 I joined CODED to master coding</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,62 @@
+const lifeTimeLine = [
+  {
+    id: 1,
+    name: "I was born in 1985",
+  },
+  {
+    id: 2,
+    name: "My first year in school was in 1991",
+  },
+  {
+    id: 3,
+    name: "I graduated from High School in 2003",
+  },
+  {
+    id: 4,
+    name: "I got married in 2004",
+  },
+  {
+    id: 5,
+    name: "In 2004 I started learning IT in Shall Institute",
+  },
+  {
+    id: 6,
+    name:
+      "In 2005 I gave birth to the most handsome boy in the whole world Mohammed",
+  },
+  {
+    id: 7,
+    name: "In 2006 I gave birth to my little princes Rayan",
+  },
+  {
+    id: 8,
+    name: " I graduated in 2007 with a diploma in Computer Skills",
+  },
+  {
+    id: 9,
+    name:
+      "In 2008 I started working in the Ministry of Social Affairs and Labor",
+  },
+  {
+    id: 10,
+    name: "In 2013 I joined Arab Open University to study business",
+  },
+  {
+    id: 11,
+    name:
+      " I changed my work place and I joined the Public Authority of Sports",
+  },
+  {
+    id: 12,
+    name: "I graduated in 2017 with a Bachelor’s in Business Administration",
+  },
+  {
+    id: 13,
+    name: " In 2019 I decided to learn coding and I started with Swift",
+  },
+  {
+    id: 14,
+    name: "In 2020 I joined CODED to master coding",
+  },
+];
+export default lifeTimeLine;
